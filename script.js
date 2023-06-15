f192b32c1bd1f639ae0c69a420343e27db0f694e
reynatojunior <reynato.junior@hotmail.com>
2023-06-15 16:55:26 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //pegar imagem
+
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+if (html.classList.contains("light")) {
+   img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+/*
+  
+  */
+ html.classList.toggle('light')
+}
